b8ea2cb213a49bcb2eecb92546db20b06f5a7c29
sarvesh rajpure <rajpuresarvesh@gmail.com>
2024-04-12 14:15:28 +0530

    updated cors for vercel deploy

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 const corsOrigin = process.env.CORS_ORIGIN;
 app.use(
   cors({
-    origin: "https://dns-manager-66rs3jll7-sarveshrajpures-projects.vercel.app/",
+    origin: ["https://dns-manager-66rs3jll7-sarveshrajpures-projects.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
